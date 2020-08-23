e93b2e4570677488eb18178292ccc27d66baa6e9
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-08-23 11:58:11 +0100

    Track built

---
+++ SuperCars2/Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float ballSpeed = 10.0f;
+    private Vector3 moveInput;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = moveInput * ballSpeed;
+    }
+}
