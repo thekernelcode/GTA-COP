8b0af00d039cd4765a2947d8ecb15bdfa04f8aeb
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2021-01-03 13:35:50 +0000

    Added in WorldMap - Outline

--- GTA-COP/Assets/Scripts/CameraFollow.cs
+++ GTA-COP/Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    private Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        startPosition = new Vector3(player.transform.position.x, 20, player.transform.position.z);
+        transform.position = startPosition;
         offset = transform.position - player.transform.position;
     }
 
